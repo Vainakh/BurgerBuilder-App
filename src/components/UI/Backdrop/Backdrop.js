631227b1683ad459585166bb475913260f87cdf7
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-01 12:04:34 -0800

    Implement backdrop component. Fix a bug with resetting total price

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import './Backdrop.css';
 
-
-
-const backdrop = () => {
-
-};
+const backdrop = (props) => (
+  props.show ? <div className={"Backdrop"} onClick={props.clicked}></div> : null
+)
 
 export default backdrop;
