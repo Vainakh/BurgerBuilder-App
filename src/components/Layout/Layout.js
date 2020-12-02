2b05611963f384fbfe2824c5149f9d07f09d557a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-02 12:40:26 -0800

    Add side drawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Aux>
     <Toolbar/>
-    <div>SideDrawer, Backdrop</div>
+    <SideDrawer/>
+    <div>Backdrop</div>
     <main className="Content">
     {props.children}
     </main>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideDrawer from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+  return (
+    <div className={"SideDrawer"}>
+      <Logo/>
+      <nav>
+        <NavigationItems/>
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
