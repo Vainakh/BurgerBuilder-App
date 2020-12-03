52f585483488d4db52be6013bf801a18d1a77a27
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-03 07:34:31 -0800

    Finish basic structure and functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Aux>
     <Toolbar/>
-    <SideDrawer/>
-    <div>Backdrop</div>
     <main className="Content">
-    {props.children}
+      {props.children}
     </main>
   </Aux> 
 )
