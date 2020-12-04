5410bca532e6f5dae6eac09f3485d11593a055c3
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-04 06:29:15 -0800

    Send post request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 
@@ -74,7 +75,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Adlan",
+        address: {
+          street: "California Street",
+          zipCode: "94108",
+          country: "USA"
+        },
+        email: "test@test.com"
+      },
+      deliveryMethod: "Fastest"
+    }
+    axios.post('/orders.json', order)
+         .then(response => console.log(response))
+         .catch(error => console.log(error));
   };
 
   render() {
