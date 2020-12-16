4ceb41c55c09dbc7d5ad7c3ae6f28ad785e4144d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-12-15 17:36:52 -0800

    Handling purchases and updaating the ui

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import './Checkout.css';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from '../Checkout/ContactData/ContactData';
 import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 
 
 class Checkout extends Component {
@@ -25,20 +26,26 @@ class Checkout extends Component {
   //   }
   //   this.setState({ingredients: ingredients, totalPrice: price})
   // }
+  // componentWillMount () {
+  //   this.props.onInitPurchase();
+  // };
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  }
+  };
 
   checkoutContinuedHandler = () => {
     this.props.history.replace('checkout/contact-data');
-  }
+  };
 
   render() {
     let summary = <Redirect to="/"/>;
+    
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary 
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -52,13 +59,20 @@ class Checkout extends Component {
       ); 
     } 
     return summary;
-  }
+  };
 };
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
-  }
-}
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
+  };
+};
 
-export default connect(mapStateToProps)(Checkout);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     onInitPurchase: () => dispatch(actions.purchaseInit())
+//   };
+// };
+
+export default connect(mapStateToProps)(Checkout);//, mapDispatchToProps
